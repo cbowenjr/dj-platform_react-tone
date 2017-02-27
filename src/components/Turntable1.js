efb6ab3720e40bd199b0e6b225ef5520cab7df31
Calvin Bowen Jr <bowenjrc@gmail.com>
2017-02-27 01:57:07 -0500

    HP LP filters

--- src/components/Turntable1.js
+++ src/components/Turntable1.js
@@ -15,27 +15,58 @@ class Turntable1 extends Component {
     this.showRecords = this.showRecords.bind(this);
     this.fetchRecord = this.fetchRecord.bind(this);
     this.play = this.play.bind(this);
-    this.keepTime = this.keepTime.bind(this)
+    this.keepTime = this.keepTime.bind(this);
+    this.raiseBMP = this.raiseBMP.bind(this);
+    this.lowerBMP = this.lowerBMP.bind(this);
+    this.raisePitch = this.raisePitch.bind(this);
+    this.lowerPitch = this.lowerPitch.bind(this);
+    this.raiseGain = this.raiseGain.bind(this);
+    this.lowerGain = this.lowerGain.bind(this);
+    this.lowPassFilter = this.lowPassFilter.bind(this);
+    this.highPassFilter = this.highPassFilter.bind(this);
+    this.delay = this.delay.bind(this);
   }
   fetchRecord(id) {
+    let gain = new Tone.Gain({
+      "gain" : 1
+    }).toMaster();
+    let filter = new Tone.Filter({
+      "frequency": 200000,
+      "type": "bandpass",
+      "rolloff" :-96,
+      "Q":0,
+      "gain":3
+    }).toMaster();
+    let feedbackDelay = new Tone.PingPongDelay({
+      "delayTime" : "8n",
+      "feedback" : 0.6,
+      "wet" : 0
+    }).toMaster();
     let player = new Tone.GrainPlayer({
       "url" : `https://a.clyp.it/rhsf3tcq.mp3`,
       "loop" : true,
       "grainSize" : 0.1,
       "overlap" : 0.05,
-    }).toMaster();
+    }).chain(gain, filter, feedbackDelay, Tone.Master);
     if (player) {
       this.setState({
-        audio:player
+        audio:player,
+        gain:gain,
+        filter:filter,
+        delay:feedbackDelay
       })
     }
   }
   changeRecord (record) {
     this.setState({
-      record:record.innerText
+      record:record.innerText,
+      count:0
     })
     document.getElementById('record1UL').classList.remove('show');
     document.getElementById('chooseRecord1').classList.remove('disappear');
+    if (this.state.playing === true) {
+      this.play();
+    }
     this.fetchRecord(record.title)
   }
   showRecords () {
@@ -75,6 +106,84 @@ class Turntable1 extends Component {
       clearInterval(timer);
     }
   }
+  raiseBMP () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.playbackRate+=.02;
+    }
+  }
+  lowerBMP () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.playbackRate-=.02;
+    }
+  }
+  raisePitch () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.detune+=30;
+    }
+  }
+  lowerPitch () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.detune-=30;
+    }
+  }
+  raiseGain () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value < 2) {
+      gain.gain.value+=.1;
+       console.log(gain.gain.value);
+      this.setState({
+        gain:gain
+      })
+    }
+  }
+  lowerGain () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value > .05) {
+          gain.gain.value-=.1;
+          this.setState({
+            gain:gain
+          })
+        }
+      }
+  lowPassFilter () {
+    let filter = this.state.filter;
+    if (this.state.audio !== 'none' && filter.type !=='lowpass') {
+      filter.type="lowpass";
+      filter.frequency.value=200;
+      filter.gain.value=4;
+      filter.Q.value=1;
+    } else if (filter.type ==='lowpass') {
+      filter.type="bandpass";
+      filter.frequency.value=200000;
+      filter.gain.value=3;
+      filter.Q.value=0;
+    }
+  }
+  highPassFilter () {
+    let filter = this.state.filter;
+    if (this.state.audio !== 'none' && filter.type!=='highpass') {
+      filter.frequency.value=700;
+      filter.type="highpass";
+    } else if (filter.type==='highpass') {
+        filter.frequency.vaue=200000;
+        filter.type="bandpass";
+
+    }
+  }
+  delay () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value < 2) {
+      gain.gain.value+=.1;
+       console.log(gain.gain.value);
+      this.setState({
+        gain:gain
+      })
+    }
+  }
   render() {
     return (
       <div>
@@ -96,26 +205,26 @@ class Turntable1 extends Component {
           <div className="mixer" id="mixer1">
             <h3>On 1</h3>
             <div className="filtersContainer">
-              <div className="filters" id="record1HP">HP</div>
-              <div className="filters" id="record1LP">LP</div>
+              <div className="filters" id="record1HP" onClick={this.highPassFilter}>HP</div>
+              <div className="filters" id="record1LP" onClick={this.lowPassFilter}>LP</div>
               <div className="filters" id="record1delay">Echo</div>
             </div>
             <div className="bpmPitch">
               <div>
-                <div className="bpmUp" id="record1bpmUp"></div>
+                <div className="bpmUp" id="record1bpmUp" onClick={this.raiseBMP}></div>
                 <p>BPM</p>
-                <div className="bpmDown" id="record1bpmDown"></div>
+                <div className="bpmDown" id="record1bpmDown" onClick={this.lowerBMP}></div>
               </div>
               <div>
-                <div className="pitchUp" id="record1pitchUp"></div>
+                <div className="pitchUp" id="record1pitchUp" onClick={this.raisePitch}></div>
                 <p>PITCH</p>
-                <div className="pitchDown" id="record1pitchDown"></div>
+                <div className="pitchDown" id="record1pitchDown" onClick={this.lowerPitch}></div>
             </div>
             </div>
             <div className="gain">
-              <div className="volumeUp" id="record1gain"></div>
+              <div className="volumeUp" id="record1gain" onClick={this.raiseGain}></div>
               <p>GAIN</p>
-              <div className="volumeDown" id="record1subgain"></div>
+              <div className="volumeDown" id="record1subgain" onClick={this.lowerGain}></div>
             </div>
             <div className="loopRew">
               <div className="rewind" id="record1rewind"></div>
--- src/components/Turntable2.js
+++ src/components/Turntable2.js
@@ -14,27 +14,58 @@ class Turntable2 extends Component {
     this.showRecords = this.showRecords.bind(this);
     this.fetchRecord = this.fetchRecord.bind(this);
     this.play = this.play.bind(this);
-    this.keepTime = this.keepTime.bind(this)
+    this.keepTime = this.keepTime.bind(this);
+    this.raiseBMP = this.raiseBMP.bind(this);
+    this.lowerBMP = this.lowerBMP.bind(this);
+    this.raisePitch = this.raisePitch.bind(this);
+    this.lowerPitch = this.lowerPitch.bind(this);
+    this.raiseGain = this.raiseGain.bind(this);
+    this.lowerGain = this.lowerGain.bind(this);
+    this.lowPassFilter = this.lowPassFilter.bind(this);
+    this.highPassFilter = this.highPassFilter.bind(this);
+    this.delay = this.delay.bind(this);
   }
   fetchRecord(id) {
+    let gain = new Tone.Gain({
+      "gain" : 1
+    }).toMaster();
+    let filter = new Tone.Filter({
+      "frequency": 200000,
+      "type": "bandpass",
+      "rolloff" :-96,
+      "Q":0,
+      "gain":3
+    }).toMaster();
+    let feedbackDelay = new Tone.PingPongDelay({
+      "delayTime" : "8n",
+      "feedback" : 0.6,
+      "wet" : 0
+    }).toMaster();
     let player = new Tone.GrainPlayer({
       "url" : `https://a.clyp.it/rhsf3tcq.mp3`,
       "loop" : true,
       "grainSize" : 0.1,
       "overlap" : 0.05,
-    }).toMaster();
+    }).chain(gain, filter, feedbackDelay, Tone.Master);
     if (player) {
       this.setState({
-        audio:player
+        audio:player,
+        gain:gain,
+        filter:filter,
+        delay:feedbackDelay
       })
     }
   }
   changeRecord (record) {
     this.setState({
-      record:record.innerText
+      record:record.innerText,
+      count:0
     })
     document.getElementById('record2UL').classList.remove('show');
     document.getElementById('chooseRecord2').classList.remove('disappear');
+    if (this.state.playing === true) {
+      this.play();
+    }
     this.fetchRecord(record.title)
   }
   showRecords () {
@@ -45,7 +76,7 @@ class Turntable2 extends Component {
     let audio = this.state.audio;
     let playButton = document.getElementById('playButton2');
     if (audio !== 'none' && playButton.innerText === 'START') {
-      audio.start("+0.0", `${this.state.count}`);
+      audio.start("+0.1", `${this.state.count}`);
       playButton.innerText = 'STOP';
       this.setState({
         playing:true
@@ -74,6 +105,84 @@ class Turntable2 extends Component {
       clearInterval(timer);
     }
   }
+  raiseBMP () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.playbackRate+=.02;
+    }
+  }
+  lowerBMP () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.playbackRate-=.02;
+    }
+  }
+  raisePitch () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.detune+=30;
+    }
+  }
+  lowerPitch () {
+    if (this.state.audio !== 'none') {
+      let audio = this.state.audio;
+      audio.detune-=30;
+    }
+  }
+  raiseGain () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value < 2) {
+      gain.gain.value+=.1;
+       console.log(gain.gain.value);
+      this.setState({
+        gain:gain
+      })
+    }
+  }
+  lowerGain () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value > .05) {
+          gain.gain.value-=.1;
+          this.setState({
+            gain:gain
+          })
+        }
+      }
+  lowPassFilter () {
+    let filter = this.state.filter;
+    if (this.state.audio !== 'none' && filter.type !=='lowpass') {
+      filter.type="lowpass";
+      filter.frequency.value=200;
+      filter.gain.value=4;
+      filter.Q.value=1;
+    } else if (filter.type ==='lowpass') {
+      filter.type="bandpass";
+      filter.frequency.value=200000;
+      filter.gain.value=3;
+      filter.Q.value=0;
+    }
+  }
+  highPassFilter () {
+    let filter = this.state.filter;
+    if (this.state.audio !== 'none' && filter.type!=='highpass') {
+      filter.frequency.value=700;
+      filter.type="highpass";
+    } else if (filter.type==='highpass') {
+        filter.frequency.vaue=200000;
+        filter.type="bandpass";
+
+    }
+  }
+  delay () {
+    let gain = this.state.gain;
+    if (this.state.audio !== 'none' && gain.gain.value < 2) {
+      gain.gain.value+=.1;
+       console.log(gain.gain.value);
+      this.setState({
+        gain:gain
+      })
+    }
+  }
   render() {
     return (
       <div>
@@ -89,29 +198,29 @@ class Turntable2 extends Component {
       </ul>
         <h2 className="chooseRecord" id="chooseRecord2" onClick={this.showRecords}>{this.state.record}</h2>
         <div className="turnMix">
-          <div className="mixer">
+          <div className="mixer" id="mixer2">
             <h3>On 2</h3>
             <div className="filtersContainer">
-              <div className="filters" id="record2HP">HP</div>
-              <div className="filters" id="record2LP">LP</div>
-              <div className="filters" id="record2echo">Echo</div>
+              <div className="filters" id="record2HP" onClick={this.highPassFilter}>HP</div>
+              <div className="filters" id="record2LP" onClick={this.lowPassFilter}>LP</div>
+              <div className="filters" id="record2delay">Echo</div>
             </div>
-               <div className="bpmPitch">
+            <div className="bpmPitch">
               <div>
-                <div className="bpmUp" id="record2bpmUp"></div>
+                <div className="bpmUp" id="record2bpmUp" onClick={this.raiseBMP}></div>
                 <p>BPM</p>
-                <div className="bpmDown" id="record2bpmDown"></div>
+                <div className="bpmDown" id="record2bpmDown" onClick={this.lowerBMP}></div>
               </div>
               <div>
-                <div className="pitchUp" id="record2pitchUp"></div>
+                <div className="pitchUp" id="record2pitchUp" onClick={this.raisePitch}></div>
                 <p>PITCH</p>
-                <div className="pitchDown" id="record2pitchDown"></div>
+                <div className="pitchDown" id="record2pitchDown" onClick={this.lowerPitch}></div>
             </div>
             </div>
             <div className="gain">
-              <div className="volumeUp" id="record2gain"></div>
+              <div className="volumeUp" id="record2gain" onClick={this.raiseGain}></div>
               <p>GAIN</p>
-              <div className="volumeDown" id="record2subgain"></div>
+              <div className="volumeDown" id="record2subgain" onClick={this.lowerGain}></div>
             </div>
             <div className="loopRew">
               <div className="rewind" id="record2rewind"></div>
